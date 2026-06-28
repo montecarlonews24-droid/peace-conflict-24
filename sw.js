19b91ecb0997606278c8b410af6c1f538510377e
montecarlonews24-droid <montecarlo.news24@gmail.com>
2026-06-28 22:26:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-// Force self-destruction
+// Force complete cache clear and unregister
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.registration.unregister())
-    .then(() => self.clients.matchAll())
-    .then(clients => clients.forEach(c => c.navigate(c.url)))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ includeUncontrolled: true }))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
+});
+self.addEventListener('fetch', (e) => {
+  // Pass everything through - no caching
+  e.respondWith(fetch(e.request));
 });
